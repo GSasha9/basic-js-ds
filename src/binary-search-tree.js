4a83bca2f74bb7bf47af05b3f9c7b86e11444eec
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2024-12-30 15:06:32 +0300

    feat: bst task. implement root, add, min, max methods. need refactor

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,48 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(data = null) {
+    if(data) {
+      this.givenRoot = new Node(data)
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    if(this.givenRoot) {
+      return this.givenRoot;
+    } else {
+      return null;
+    }
+  }
+
+  add(data) {
+    let newNode = new Node(data);
+
+    function searchTree(node) {
+      if(data < node.value) {
+        if(!node.left) {
+          node.left = newNode;
+        } else {
+          searchTree(node.left);
+        }
+      } else if (data > node.value) {
+        if(!node.right) {
+          node.right = newNode;
+        } else {
+          searchTree(node.right);
+        }
+      }
+    }
+
+    if(this.givenRoot){
+      searchTree(this.givenRoot);
+    }
   }
 
   has(/* data */) {
@@ -34,13 +61,23 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.givenRoot) {
+      let current = this.givenRoot;
+      while(current.left) {
+        current = current.left;
+      }
+      return current.value;
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.givenRoot) {
+      let current = this.givenRoot;
+      while(current.right) {
+        current = current.right;
+      }
+      return current.value;
+    }
   }
 }
 
